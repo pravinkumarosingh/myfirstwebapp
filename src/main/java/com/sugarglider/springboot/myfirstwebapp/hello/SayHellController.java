6313cab1c135985d82bfb5a461487d3723ed4a8b
pravinkumarosingh <headfryer@gmail.com>
2022-10-30 17:15:14 +0530

    say hello controller added.

---
+++ src/main/java/com/sugarglider/springboot/myfirstwebapp/hello/SayHellController.java
@@ -0,0 +1,15 @@
+package com.sugarglider.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHellController {
+
+    @RequestMapping("say-hello")
+    @ResponseBody()
+    public String sayHello(){
+        return "Hello, What are you learning today?";
+    }
+}
